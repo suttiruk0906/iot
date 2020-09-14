f455ef7ab739303b550308e8f6f36406a859ddfd
suttiruk0906 <suttiruk0906@gmail.com>
2020-09-14 15:40:14 +0700

    Hello World

---
+++ android/app/src/main/kotlin/com/suttiruk/iot/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.suttiruk.iot
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+main() {
+  runApp(Myapps());//main apps
+}
+class Myapps extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Text('Hello world'),//home apps
+    );
+  }
+}
